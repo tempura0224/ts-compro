e676c5be37d928e6a6e9389cfbac1dc8ea6636ac
tempura <tempura0224@gmail.com>
2021-04-20 02:27:43 +0900

    初期設定とテンプレート作成

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import * as fs from "fs";
+
+class Input {
+	private words: string[];
+	private idx: number;
+	constructor(rawText: string) {
+		this.idx = 0;
+		this.words = rawText.split(/\s/);
+	}
+	nextWord = () => this.words[this.idx++];
+	nextWords = (n: number) => {
+		const ret = this.words.slice(this.idx, this.idx + n);
+		this.idx += n;
+		return ret;
+	};
+	nextInt = () => Number(this.words[this.idx++]);
+	nextInts = (n: number) => {
+		const ret = this.words.slice(this.idx, this.idx + n).map(Number);
+		this.idx += n;
+		return ret;
+	};
+	nextLong = () => BigInt(this.words[this.idx++]);
+	nextLongs = (n: number) => {
+		const ret = this.words.slice(this.idx, this.idx + n).map(BigInt);
+		this.idx += n;
+		return ret;
+	};
+}
+
+const solve = (input: Input) => {
+	const [x, y, z] = input.nextInts(3);
+	return Math.floor((y * z - 1) / x);
+};
+
+const main = () => {
+	const inputText = fs.readFileSync("/dev/stdin", "utf8");
+	const input = new Input(inputText);
+	const ans = solve(input);
+	console.log(ans);
+};
+
+main();
